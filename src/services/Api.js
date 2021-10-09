90910e805b054e761143f550438bcb5db99a2993
cervantes2907 <82742678+cervantes2907@users.noreply.github.com>
2021-10-09 03:11:17 -0500

    headers

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export const Api = axios.create({
-    baseURL: 'http://34.67.224.76'
+    baseURL: 'http://34.67.224.76',
+    headers: {'Content-Type': 'application/json'}
 });
